471005d10869fdf8a66fafd55b03fe3c0bdb4090
ArshM17 <arshmaknojia5@gmail.com>
2023-01-08 16:57:50 +0530

    created ideapage folder

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './home.css';
 import ChampCard from "../components/championCard";
+import IdeaList from './idea_Page/ideaList';
 
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
                 <h1 className='titleText'>Welcome to <br/><span className='span1'>BridgeStone<br/></span> <span className='span2'>Idea Submission Platform</span> </h1>
             </div>
             <ChampCard/>
+            <IdeaList/>
         </>
     )
 }
