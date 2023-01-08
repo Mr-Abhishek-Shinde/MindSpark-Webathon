bac687cf434489f665a7559a0a32a0bf79c0b606
Abhishek <shindeaa21.comp@coep.ac.in>
2023-01-08 15:37:07 +0530

    added sideNav component

--- frontend/src/components/idea_Page/sideNav.js
+++ frontend/src/components/idea_Page/sideNav.js
@@ -1,21 +1,64 @@
-import React, { useState } from 'react';
-import Popup from './popup'
-import IdeaForm from './ideaForm'
 import './sideNav.css';
+import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
+import { useState } from 'react';
+import homeImg from './img/icons8-home-24.png';
+import infoImg from './img/icons8-info-squared-24.png';
+import resumeImg from './img/icons8-profiles-24.png';
+import projectsImg from './img/icons8-learning-24.png';
+import contactImg from './img/icons8-contact-24.png';
 
 
-function Navbar() {
-    const [btnPopup, setbtnPopup] = useState(false);
-    const [ideaForm, setideaForm] = useState(false);
+export default function Navbar() {
+    const [Show, setShow] = useState(0)
+    function navFxn(menuu){
+        if (Show === 0) {
+            setShow(-100);
+            //remove class
+            menuu.currentTarget.classList.remove('chng')
+        } else {
+            setShow(0);
+            //add class
+            menuu.currentTarget.classList.add('chng')
+        }
+    }
+
+    function handleResize() {
+        if (window.innerWidth < 690) {
+            setShow(-100);
+            //remove class
+        }
+        else{
+            setShow(0);
+            //add class
+        }
+    }
+
+    window.addEventListener('resize', handleResize)
 
     return (
-        <div className="navbar">
-            <div className="loginBtn" onClick={() => setideaForm(true)}>Apply</div>
-            <div className="loginBtn" onClick={() => setbtnPopup(true)}>User Login</div>
-            <Popup trigger={btnPopup} setTrigger={setbtnPopup} />
-            <IdeaForm trigger={ideaForm} setTrigger={setideaForm} />
-        </div>
+        <>
+            <div className="sideBar" id="side" style={{left: `${Show}%`}}>
+                <div className="logo">
+                    <a href="/"><span>P</span>ortfolio</a>
+                </div>
+                <div className="nav">
+                    <li id="navBtn" onClick={handleResize}><NavLink end to='/'><img src={homeImg}
+                                alt=""/> Home</NavLink></li>
+                    <li id="navBtn" onClick={handleResize}><NavLink end to='/about'><img src={infoImg}
+                                alt=""/> About</NavLink></li>
+                    <li id="navBtn" onClick={handleResize}><NavLink end to='/resume'><img src={resumeImg}
+                                alt=""/> Resume</NavLink></li>
+                    <li id="navBtn" onClick={handleResize}><NavLink end to='/projects'><img src={projectsImg}
+                                alt=""/> Projects</NavLink></li>
+                    <li id="navBtn" onClick={handleResize}><NavLink end to='/contact'><img src={contactImg}
+                                alt=""/> Contact</NavLink></li>
+                </div>
+            </div>
+            <div className="nav-toggler" id="toggleBtn1" onClick={navFxn}>
+                <span></span>
+            </div>
+            <Outlet/>
+        </>
     )
 }
-
-export default Navbar
