133a9733b5fbdfabbf6c6c728969316599962eaf
Himanshu <hiifriends366@gmail.com>
2023-01-09 23:45:47 +0530

    button in program details

--- frontend/src/components/programDetails.js
+++ frontend/src/components/programDetails.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import cardImg from '../img/wallpaper.jpg';
-// import VisibilityIcon from '@mui/icons-material/Visibility';
-// import PersonIcon from '@mui/icons-material/Person';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import PersonIcon from '@mui/icons-material/Person';
 import './programDetails.css';
+import { Button } from '@mui/material';
 
 
 
@@ -15,21 +16,32 @@ function ProgramDetails() {
       <div className="info-container">
         <h3>Bridgestone- Marketing</h3>
         <p>Idea submission for Marketing dept.</p>
+        <div className="pro-duration">
         <h4>Program Duration Check Timeline </h4>
-        <p>Date to Date</p>
+        <p>Date to Date</p></div>
         <hr />
         <h4>Idea Submission</h4>
-        <p>Date to Date</p> 
-        <hr/>
+        <p>Date to Date</p>
+        <hr />
         <h4>Interseted | Visitors</h4>
-        {/* <span><PersonIcon/>&nbsp;</span> */}
+        <div className="visitors">
+        <span><PersonIcon />&nbsp;</span>
         <span>0 &nbsp;</span>
-        {/* <span><VisibilityIcon/>&nbsp;</span> */}
-        <span>4</span>
+        <span><VisibilityIcon />&nbsp;</span>
+        <span>4</span>   </div> 
+        <hr />
+        <div className="info-tags">
+          <span>#EmployeeEngagement</span>
+          <span>#AICognitive</span>
+          <span>#Test</span>
+          +2 more</div>    
+        <Button variant="contained" color="primary" onClick={() => {
+          alert('clicked');
+        }}>Apply</Button>
       </div>
     </div>
-    
-    )
+
+  )
 }
 
 export default ProgramDetails;
