725e90fd80159dc545d101ad5f1d95fc3fe0bb05
Himanshu <hiifriends366@gmail.com>
2023-01-09 17:56:35 +0530

    Program Details

--- frontend/src/components/programDetails.js
+++ frontend/src/components/programDetails.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import cardImg from '../img/wallpaper.jpg';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import PersonIcon from '@mui/icons-material/Person';
+import './programDetails.css';
 
-function programDetails() {
+
+
+function ProgramDetails() {
   return (
-    <div>programDetails</div>
-  )
+    <div className="details-container">
+      <div className="image-container">
+        <img src={cardImg} alt="" />
+      </div>
+      <div className="info-container">
+        <h3>Bridgestone- Marketing</h3>
+        <p>Idea submission for Marketing dept.</p>
+        <h4>Program Duration Check Timeline </h4>
+        <p>Date to Date</p>
+        <hr />
+        <h4>Idea Submission</h4>
+        <p>Date to Date</p> 
+        <hr/>
+        <h4>Interseted | Visitors</h4>
+        <span><PersonIcon/>&nbsp;</span>
+        <span>0 &nbsp;</span>
+        <span><VisibilityIcon/>&nbsp;</span>
+        <span>4</span>
+      </div>
+    </div>
+    
+    )
 }
 
-export default programDetails
+export default ProgramDetails;
