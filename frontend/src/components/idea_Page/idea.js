56a9f61f94e7d3f19680293f4bc8e625b8ec50f3
ArshM17 <arshmaknojia5@gmail.com>
2023-01-09 11:53:57 +0530

    Styled idea page

--- frontend/src/components/idea_Page/idea.js
+++ frontend/src/components/idea_Page/idea.js
@@ -4,15 +4,20 @@ import './idea.css'
 function Idea() {
   return (
     <>
-        <h1>Title</h1>
-        <input type = "checkbox"/><span>Application Stage</span>
-        <div>
-            <ul>
-                <li>Invite</li>
-                <li>Add/Edit</li>
-                <li>Delete</li>
-                <li>Details</li>
-            </ul>
+    <div className='idea-container'>
+        <h3>Title</h3>
+        <div className='wrapper'>
+            <div>
+                <input className='check' type = "checkbox"/><span>Application Stage</span>
+            </div>
+            <div className='button-container'>
+                <ul>
+                    <li>Invite</li>
+                    <li>Add/Edit</li>
+                    <li className='delete'>Delete</li>
+                    <li>Details</li>
+                </ul>
+            </div>
         </div>
         <hr/>
         <div>
@@ -44,6 +49,7 @@ function Idea() {
                 </li>
             </ul>
         </div>
+    </div>
     </>
   )
 }
