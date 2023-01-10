30abb2fadefd75329dde305bf3fcb8f9c2222128
Swarnim-K <kambless.elec@coep.ac.in>
2023-01-10 21:07:48 +0530

    adding program model

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,6 +2,31 @@ from django.db import models
 from django.contrib.auth.hashers import make_password
 
 # Create your models here.
+class Admin(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    username = models.CharField(max_length=30, blank=True)
+    email = models.EmailField(max_length=254, null=True)
+    password = models.CharField(max_length=30, blank=True)
+    role = models.CharField(max_length=30)
+    date_joined =  models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        full_name = self.first_name + " " + self.last_name
+        return full_name
+
+class Program(models.Model):
+    title = models.CharField(max_length=50)
+    subtitle = models.CharField(max_length=100)
+    image = models.CharField(max_length=200, default="https://i0.wp.com/t-artmagazine.com/wp-content/uploads/2021/04/vector-creator.png?fit=800%2C534&ssl=1")
+    starting_date = models.DateTimeField(default=None)
+    submission_date = models.DateTimeField(default=None)
+    author = models.ForeignKey(Admin, on_delete=models.CASCADE, default=None)
+    created =  models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
 
 class Ideator(models.Model):
     first_name = models.CharField(max_length=30)
@@ -20,6 +45,8 @@ class Ideator(models.Model):
 class Idea(models.Model):
     title = models.CharField(max_length=30)
     body = models.TextField()
+    program = models.ForeignKey(Program, on_delete=models.CASCADE, default=None)
+    image = models.CharField(max_length=200, default="https://i0.wp.com/t-artmagazine.com/wp-content/uploads/2021/04/vector-creator.png?fit=800%2C534&ssl=1")
     author = models.ForeignKey(Ideator, on_delete=models.CASCADE, default=None)
     created =  models.DateTimeField(auto_now_add=True)
 
@@ -40,15 +67,3 @@ class Innovation_Champion(models.Model):
         full_name = self.first_name + " " + self.last_name
         return full_name
 
-class Admin(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    username = models.CharField(max_length=30, blank=True)
-    email = models.EmailField(max_length=254, null=True)
-    password = models.CharField(max_length=30, blank=True)
-    role = models.CharField(max_length=30)
-    date_joined =  models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        full_name = self.first_name + " " + self.last_name
-        return full_name
--- backend/api/views.py
+++ backend/api/views.py
@@ -6,8 +6,8 @@ from rest_framework.decorators import api_view
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 
-from .models import Ideator, Innovation_Champion, Admin, Idea
-from .serializers import IdeatorSerializer, UserSerializer, InnovationChampionSerializer, AdminSerializer, IdeaSerializer
+from .models import Ideator, Innovation_Champion, Admin, Idea, Program
+from .serializers import IdeatorSerializer, UserSerializer, InnovationChampionSerializer, AdminSerializer, IdeaSerializer, ProgramSerializer
 
 # Create your views here.
 
@@ -206,4 +206,10 @@ def loginUser(request):
 def getIdeas(request):
     ideas = Idea.objects.all()
     serializer = IdeaSerializer(ideas, many=True)
+    return Response(serializer.data)
+
+@api_view(["GET"])
+def getPrograms(request):
+    programs = Program.objects.all()
+    serializer = ProgramSerializer(programs, many=True)
     return Response(serializer.data)
