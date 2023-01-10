82ff1bd787a0432f4a8bf94a75d910fd7b6a5297
Swarnim-K <kambless.elec@coep.ac.in>
2023-01-11 01:36:33 +0530

    adding emailjs

--- frontend/src/components/innovationChampionPage/idea.js
+++ frontend/src/components/innovationChampionPage/idea.js
@@ -1,8 +1,49 @@
-import React from 'react'
+import { React, useRef } from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './idea.css'
 import userImage from "../../img/userImage.png"
+import emailjs from '@emailjs/browser';
 
 function Idea({ idea }) {
+    const form = useRef();
+
+    const sendEmail1 = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_cjb82yu', 'template_3rm7ub8', form.current, 'FvEdaxdn0Br206UYe')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+
+        console.log("Email sent");
+        notifyAccept();
+    };
+
+    const sendEmail2 = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_cjb82yu', 'template_wncgbku', form.current, 'FvEdaxdn0Br206UYe')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+
+        console.log("Email sent");
+        notifyReject();
+    };
+
+    const notifyAccept = () => {
+        toast("acception email sent to ideator successfully!!");
+    }
+
+    const notifyReject = () => {
+        toast("Rejection email sent to ideator successfully!!");
+    }
+
     return (
         <>
             <div className='idea-container'>
@@ -21,13 +62,44 @@ function Idea({ idea }) {
                     </div>
                 </div>
                 <div className='button-wrap'>
-                    <span className='idea-btn accept'>Accept</span>
+                    <form className="accept-form" ref={form} onSubmit="">
+                        <div className="input-section2">
+                            <input type="text" name="fullName" id="fullName" placeholder="Full Name*" value={idea.author[0].name} />
+                        </div>
+                        <div className="input-section2">
+                            <input type="email" name="user_email" id="email" placeholder="Email*" value={idea.author[0].email} />
+                        </div>
+                        <div className="input-section2">
+                            <input type="text" name="idea" id="idea" placeholder="title*" value={idea.title} />
+                        </div>
+                        {/* <div className="input-section2">
+                            <input type="text" name="status" id="status" placeholder="title*" value="accepted" />
+                        </div> */}
+                        <span className='idea-btn accept' onClick={sendEmail1}>Accept</span>
+                    </form>
+
                     <span className='idea-btn hold'>Hold</span>
-                    <span className='idea-btn reject'>Reject</span>
+
+                    <form className="accept-form" ref={form} onSubmit="">
+                        <div className="input-section2">
+                            <input type="text" name="fullName" id="fullName" placeholder="Full Name*" value={idea.author[0].name} />
+                        </div>
+                        <div className="input-section2">
+                            <input type="email" name="user_email" id="email" placeholder="Email*" value={idea.author[0].email} />
+                        </div>
+                        <div className="input-section2">
+                            <input type="text" name="idea" id="idea" placeholder="title*" value={idea.title} />
+                        </div>
+                        {/* <div className="input-section2">
+                            <input type="text" name="status" id="status" placeholder="title*" value="rejected" />
+                        </div> */}
+                        <span className='idea-btn reject' onClick={sendEmail2}>Reject</span>
+                    </form>
                 </div>
+                <ToastContainer />
             </div>
         </>
     )
 }
 
-export default Idea
+export default Idea;
