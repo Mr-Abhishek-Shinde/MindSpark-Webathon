174004b81ec9a053816c97376a8988705c943103
Abhishek <shindeaa21.comp@coep.ac.in>
2023-01-12 20:44:47 +0530

    changed overall styling

--- frontend/src/pages/ideator.js
+++ frontend/src/pages/ideator.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Navbar from '../components/homePage/navbar';
 import ChampCard from "../components/ideatorPage/championCard";
 import ProgramCard from "../components/ideatorPage/programCard";
-import { Route, Link, Routes, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 export default function Ideator() {
 
     const params = useParams();
 
-    //console.log(params);
+    console.log(params);
 
     return (
         <>
